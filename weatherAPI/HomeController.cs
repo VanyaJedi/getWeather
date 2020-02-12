bae0dbcf69209cd0e9d9ef7a28d54ac0fdc1a01f
Ivan <vanya71310@gmail.com>
2020-02-12 14:16:16 +0300

    Add repo

---
+++ weatherAPI/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace weatherAPI
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+
+        public string getApiWeather(string city)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=085f4b50e8adc99d36594bccf45dfcf3");
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                /*WebExceptionStatus status = ex.Status;
+                if (status == WebExceptionStatus.ProtocolError)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse)ex.Response;
+                    return "123";
+                }*/
+                return "error";
+            }
+           
+
+        }
+    }
+}
---
+++ weatherAPI/wwwroot/script.js
@@ -0,0 +1,58 @@
+﻿
+'use strict'
+
+var getBtn = document.querySelector('.button');
+var form = document.querySelector('.form');
+var cityInput = document.querySelector('.city');
+var data = document.querySelector('.data');
+
+var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://localhost:44391/Home/getApiWeather?city=' + cityInput.value);
+
+    xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+            onLoad(xhr.response);
+        } else {
+            onError('Ошибка ' + xhr.status + ' ' + xhr.statusText);
+        }
+    });
+
+    xhr.addEventListener('error', function () {
+        onError('Ошибка соединения');
+    });
+
+    xhr.send();
+}
+
+var getData = function (response) {
+    if (response === 'error') {
+        cityInput.classList.add('city-error');
+        cityInput.value = 'city not found';
+        return;
+    }
+    cityInput.classList.remove('city-error');
+    cityInput.value = '';
+    var temperature = JSON.parse(response);
+    console.log(temperature);
+    document.querySelector('.data-result-city').innerText = temperature.name;
+    document.querySelector('.data-result-temp').innerText = temperature.main.temp;
+    document.querySelector('.data-result-wind').innerText = temperature.wind.speed;
+    data.classList.remove('hidden');
+}
+
+
+var showError = function (message) {
+    alert(message);
+}
+
+form.addEventListener('submit', function (evt) {
+    data.classList.add('hidden');
+    evt.preventDefault();
+    load(getData, showError);
+});
+
+
+cityInput.addEventListener('input', function () {
+    cityInput.classList.remove('city-error');
+});
